bbc878433f0e44a9a2c31ebd5b4f1165c0542439
DiegoCentanni <centanni.diego@iisgalvanimi.edu.it>
2026-05-05 07:43:05 +0000

    Ninja Finitoo

--- ninja.js
+++ ninja.js
@@ -1,16 +1,16 @@
-var babbo;
+var ninja;
 
 function startGame() {
     myGameArea.start();
-    babbo = new component(120, 120, 10, 120);
-    babbo.loadImages();
+    ninja = new CreaNinja(120, 120, 10, 120);
+    ninja.loadImages();
 }
 
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function() {
-        this.canvas.width = 790;
-        this.canvas.height = 370;
+        this.canvas.width = 800;
+        this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.getElementById("canvas-container").appendChild(this.canvas);
         this.interval = setInterval(updateGameArea, 20);
@@ -20,7 +20,7 @@ var myGameArea = {
     }
 };
 
-function component(width, height, x, y) {
+function CreaNinja(width, height, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
@@ -28,8 +28,8 @@ function component(width, height, x, y) {
     this.speedX = 0;
     this.speedY = 0;
     this.imageList = [];
-    this.actualFrame = 0;
-    this.contaFrame = 0;
+    this.FotogrammaMostrato = 0;
+    this.RallentaAnimazione = 0;
 
     this.loadImages = function() {
         for (let i = 0; i < running.length; i++) {
@@ -46,40 +46,26 @@ function component(width, height, x, y) {
 
     this.update = function() {
         let ctx = myGameArea.context;
-
         if (this.speedX !== 0 || this.speedY !== 0) {
-            this.contaFrame++;
-            if (this.contaFrame >= 6) {
-                this.contaFrame = 0;
-                this.actualFrame = (this.actualFrame + 1) % this.imageList.length;
+            this.RallentaAnimazione++;
+            if (this.RallentaAnimazione >= 6) {
+                this.RallentaAnimazione = 0;
+                this.FotogrammaMostrato = (this.FotogrammaMostrato + 1) % this.imageList.length;
             }
         }
-
-        let imgAttuale = this.imageList[this.actualFrame];
-        if (imgAttuale) {
-            ctx.drawImage(imgAttuale, this.x, this.y, this.width, this.height);
-        }
+        let imgAttuale = this.imageList[this.FotogrammaMostrato];
+        if (imgAttuale) ctx.drawImage(imgAttuale, this.x, this.y, this.width, this.height);
     };
 }
 
 function updateGameArea() {
-    myGameArea.clear();    
-    babbo.newPos();        
-    babbo.update();        
+    myGameArea.clear();
+    ninja.newPos();
+    ninja.update();
 }
 
-function moveup() {
-    babbo.speedY = -3;
- }
-function movedown() {
-    babbo.speedY = 3;
- }
-function moveleft() {
-    babbo.speedX = -3;
-}
-function moveright() {
-    babbo.speedX = 3;
- }
-function clearmove() {
-    babbo.speedX = 0; babbo.speedY = 0;
-}
+function moveup()    { ninja.speedY = -3; }
+function movedown()  { ninja.speedY =  3; }
+function moveleft()  { ninja.speedX = -3; }
+function moveright() { ninja.speedX =  3; }
+function NinjaFermo(){ ninja.speedX = 0; ninja.speedY = 0; }
